47f36869c7b156ef2bb005f7bfaca0b7b583da3d
Aaron Johnston <aaron.johnston001@gmail.com>
2024-05-21 00:52:52 -0700

    Make attention model into a class

--- attention.py
+++ attention.py
@@ -1,13 +1,15 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained('gpt2')
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForCausalLM.from_pretrained('gpt2')
 
+class Model:
+    def __init__(self, model_name='gpt2'):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
 
-def generate_attention(text):
-    """Generates attention for a given text."""
-    inputs = tokenizer.encode(text, return_tensors='pt')
-    outputs = model(inputs, output_attentions=True)
-    return outputs.attentions[0]
+    def generate_attention(self, text):
+        """Generates attention for a given text."""
+        input_tokens = self.tokenizer.encode(text, return_tensors='pt')
+        outputs = self.model(input_tokens, output_attentions=True)
+        return input_tokens, outputs.attentions[0]
